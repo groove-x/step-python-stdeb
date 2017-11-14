4d8be51b160e1ad923cfbceb89a422ee4690ddf4
Yuma.M <yuma-m@users.noreply.github.com>
2017-11-14 16:04:55 +0900

    Update date format

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 apt-get install -y devscripts fakeroot python-stdeb
 
-NOW=$(date +'%Y%m%d-%H%M%S')
+NOW=$(date -u +'%Y%m%dT%H%M%SZ')
 cd ${WERCKER_PYTHON_STDEB_PROJECT_ROOT}
 
 python setup.py --command-package=stdeb.command debianize
