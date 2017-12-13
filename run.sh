aacdb8da5f478524a5f0dabac738ee8b383805a9
Yuma.M <yuma-m@users.noreply.github.com>
2017-12-13 09:57:28 +0900

    Add echo command

--- run.sh
+++ run.sh
@@ -9,12 +9,14 @@ PYBUILD_DIR="${WERCKER_PYTHON_STDEB_PROJECT_ROOT}/.pybuild"
 
 if [ "${WERCKER_PYTHON_STDEB_CLEAR_DEBIAN_SOURCE_DIR}" != "false" ]; then
   if [ -e ${DEBIAN_SOURCE_DIR} ]; then
+    echo "rm -rf ${DEBIAN_SOURCE_DIR}"
     rm -rf ${DEBIAN_SOURCE_DIR}
   fi
 fi
 
 if [ "${WERCKER_PYTHON_STDEB_CLEAR_PYBUILD_DIR}" != "false" ]; then
   if [ -e ${PYBUILD_DIR} ]; then
+    echo "rm -rf ${PYBUILD_DIR}"
     rm -rf ${PYBUILD_DIR}
   fi
 fi
@@ -22,6 +24,7 @@ fi
 NOW=$(date -u +'%Y%m%dT%H%M%SZ')
 cd ${WERCKER_PYTHON_STDEB_PROJECT_ROOT}
 
+echo "python setup.py --command-package=stdeb.command ${WERCKER_PYTHON_STDEB_STDEB_COMMAND}"
 python setup.py --command-package=stdeb.command ${WERCKER_PYTHON_STDEB_STDEB_COMMAND}
 
 if [ "${WERCKER_PYTHON_STDEB_STDEB_COMMAND}" == "debianize" ]; then
@@ -29,7 +32,9 @@ if [ "${WERCKER_PYTHON_STDEB_STDEB_COMMAND}" == "debianize" ]; then
 fi
 
 if [ "$WERCKER_PYTHON_STDEB_CLEAN_DEBIAN_RULES" != "false" ]; then
-fakeroot debian/rules clean
+  echo "fakeroot debian/rules clean"
+  fakeroot debian/rules clean
 fi
 
+echo "fakeroot debian/rules binary"
 fakeroot debian/rules binary
