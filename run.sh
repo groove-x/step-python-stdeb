3ac1aca3e426dd727a76a0f0527b91e105608b45
John Belmonte <john.belmonte@groove-x.com>
2018-06-08 16:28:16 +0900

    use "set -x" subshell to echo commands

--- run.sh
+++ run.sh
@@ -9,32 +9,27 @@ PYBUILD_DIR="${WERCKER_PYTHON_STDEB_PROJECT_ROOT}/.pybuild"
 
 if [ "${WERCKER_PYTHON_STDEB_CLEAR_DEBIAN_SOURCE_DIR}" != "false" ]; then
   if [ -e ${DEBIAN_SOURCE_DIR} ]; then
-    echo "rm -rf ${DEBIAN_SOURCE_DIR}"
-    rm -rf ${DEBIAN_SOURCE_DIR}
+    (set -x; rm -rf ${DEBIAN_SOURCE_DIR})
   fi
 fi
 
 if [ "${WERCKER_PYTHON_STDEB_CLEAR_PYBUILD_DIR}" != "false" ]; then
   if [ -e ${PYBUILD_DIR} ]; then
-    echo "rm -rf ${PYBUILD_DIR}"
-    rm -rf ${PYBUILD_DIR}
+    (set -x; rm -rf ${PYBUILD_DIR})
   fi
 fi
 
 NOW=$(date -u +'%Y%m%dT%H%M%SZ')
 cd ${WERCKER_PYTHON_STDEB_PROJECT_ROOT}
 
-echo "python setup.py --command-package=stdeb.command ${WERCKER_PYTHON_STDEB_STDEB_COMMAND}"
-python setup.py --command-package=stdeb.command ${WERCKER_PYTHON_STDEB_STDEB_COMMAND}
+(set -x; python setup.py --command-package=stdeb.command ${WERCKER_PYTHON_STDEB_STDEB_COMMAND})
 
 if [ "${WERCKER_PYTHON_STDEB_STDEB_COMMAND}" == "debianize" ]; then
   sed -i -e "s/-1)/-${NOW})/" debian/changelog
 fi
 
 if [ "$WERCKER_PYTHON_STDEB_CLEAN_DEBIAN_RULES" != "false" ]; then
-  echo "fakeroot debian/rules clean"
-  fakeroot debian/rules clean
+  (set -x; fakeroot debian/rules clean)
 fi
 
-echo "fakeroot debian/rules binary"
-fakeroot debian/rules binary
+(set -x; fakeroot debian/rules binary)
